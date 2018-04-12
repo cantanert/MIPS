ddea52905f78e90dc7c94ff8fa70bd92d65839d6
Can Taner TARHAN <cantanert@gmail.com>
2018-04-12 22:05:05 +0300

    Update DataTypes.s

--- DataTypes.s
+++ DataTypes.s
@@ -1,17 +1,18 @@
-				.data
-a1:			.ascii		"123"
-a2:			.ascii 		"5"
-a3:			.ascii 		"4"
+			.data
+				
+a1:		.ascii		"123"
+a2:		.ascii 		"5"
+a3:		.ascii 		"4"
 name:		.ascii 		"Can Taner"
 course: 	.ascii		"COMU C.E"
 
-b1:			.asciiz		"123"
-b2:			.asciiz		"5"
-b3:			.asciiz		"4"
+b1:		.asciiz		"123"
+b2:		.asciiz		"5"
+b3:		.asciiz		"4"
 nname:		.asciiz		"Can Taner"
 ccourse: 	.asciiz		"COMU C.E"
 
-				.text
+			.text
 
 	.globl my_main
 
