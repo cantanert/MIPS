973c8af6e06eb2114df2d8f0b529a99fcf088177
Can Taner TARHAN <cantanert@gmail.com>
2018-04-12 22:07:05 +0300

    Update loadWord.s

--- loadWord.s
+++ loadWord.s
@@ -5,16 +5,16 @@ temp:	.ascii	"0123456789ABCDEF"
 
 			.text
 
-			.globl my_main
+		.globl my_main
 
 my_main:	
-			li	$s0, 0x10010000   #taban adresimiz
-			lw	$t1, 0($s0)
+		li	$s0, 0x10010000   #taban adresimiz
+		lw	$t1, 0($s0)
 
-			lw $t2, 4($s0)			#R[$t2] = M[ R[$s0] +4]
-			lw $t2, 8($s0)			#R[$t2] = M[ R[$s0] +8]
-			lw $t2, 12($s0)			#R[$t2] = M[ R[$s0] +12]
-			lw $t2, 0x0c($s0)		#R[$t2] = M[ R[$s0] +12]
+		lw $t2, 4($s0)			#R[$t2] = M[ R[$s0] +4]
+		lw $t2, 8($s0)			#R[$t2] = M[ R[$s0] +8]
+		lw $t2, 12($s0)			#R[$t2] = M[ R[$s0] +12]
+		lw $t2, 0x0c($s0)		#R[$t2] = M[ R[$s0] +12]
 
-			li $v0, 10
-			syscall
+		li $v0, 10
+		syscall
