68c95a99ba568d462d8357a9d51bc6c6fefbbe10
Can Taner TARHAN <cantanert@gmail.com>
2018-04-12 22:09:11 +0300

    Update variableSwapWithTempRegister.s

--- variableSwapWithTempRegister.s
+++ variableSwapWithTempRegister.s
@@ -1,15 +1,17 @@
-.text
+		.text
+		
+	.globl my_main
+	
+my_main:
+	li $t0, 0x100
+	li $t1, 0x200
 
-	main:
-li $t0, 0x100
-li $t1, 0x200
-
-#li $t0, 0x200
-#li $t1, 0x100
-move $t2, $t0
-move $t0, $t1
-move $t1, $t2
+	#li $t0, 0x200
+	#li $t1, 0x100
+	move $t2, $t0
+	move $t0, $t1
+	move $t1, $t2
 
 
-li $v0, 10
-syscall
+	li $v0, 10
+	syscall
